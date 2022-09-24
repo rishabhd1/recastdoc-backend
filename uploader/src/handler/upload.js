aeec4fa9c8d5c3a6c6af49e5fe326a1fbc883d8d
Rishabh Dubey <rishabhdubey020@gmail.com>
2022-09-24 23:34:28 +0530

    added error message

--- uploader/src/handler/upload.js
+++ uploader/src/handler/upload.js
@@ -1,24 +1,29 @@
 const OriginalFile = require('../models/OriginalFile');
 
 async function upload(req, res) {
-  const urls = req.files.map(async (file) => {
-    const newFile = {
-      url: file.location,
-      name: file.key,
-      type: file.mimetype,
-      size: file.size,
-    };
+  try {
+    const urls = req.files.map(async (file) => {
+      const newFile = {
+        url: file.location,
+        name: file.key,
+        type: file.mimetype,
+        size: file.size,
+      };
 
-    await OriginalFile.create(newFile);
+      await OriginalFile.create(newFile);
 
-    return newFile;
-  });
+      return newFile;
+    });
 
-  const responseURLs = await Promise.all(urls);
+    const responseURLs = await Promise.all(urls);
 
-  console.log(JSON.stringify(responseURLs));
-
-  res.status(200).json(responseURLs);
+    res.status(200).json(responseURLs);
+  } catch (error) {
+    console.log(error);
+    res.status(502).json({
+      message: 'Internal Server Error',
+    });
+  }
 }
 
 module.exports = {
